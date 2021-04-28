ffb777f6c349ebddf4e57b2aeb16c2b21df2dc1e
Justin-Cordero <justincordero@ymail.com>
2021-04-28 17:54:01 -0400

    added env file to hide secret key

--- bug_tracker_api/settings.py
+++ bug_tracker_api/settings.py
@@ -11,7 +11,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 import datetime
+
+# Hides env variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@vtzb-+(@@2)tif%3rdmznv(5^00p11g+8dkviy@k%3tda-1p#'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
